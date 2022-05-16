824a9252e15082faeea8336b2b11571fa688d6c0
Clinton Oppong <oppongc1@my.erau.edu>
2022-05-16 17:52:14 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -304,8 +304,26 @@ public class Main {
 
     }
 
-    public static  int getEvenDigitSum(int n){
+    public static int getEvenDigitSum(int number) {
 
+        int total = 0;
+
+        if(number >= 0) {
+
+            while (number != 0){
+                int even = number % 10;
+
+                if (even % 2 == 0) {
+                    total += even;
+                }
+
+                number /= 10;
+            }
+
+            return total;
+        }
+
+        return -1;
     }
 
 }
